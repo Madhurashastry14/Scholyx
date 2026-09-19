750060f3248c8f83d716af4a3710c29992449498
Madhura Shastry <madhurashastry01@gmail.com>
2026-09-19 15:37:10 +0530

    Initial project setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Scholyx</h1>
+      <p>Scholarship Information and Eligibility Checker</p>
+    </div>
+  );
+}
+
+export default Home;
